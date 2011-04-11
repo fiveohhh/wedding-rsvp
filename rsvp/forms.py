85a68b491888afe57b085774f40aef164d1f4acd
Andy Lee <andy@chiefmarley.com>
2011-04-10 22:53:36 -0500

    working with adult and children attending in dropdown boxes.

--- rsvp/forms.py
+++ rsvp/forms.py
@@ -1,7 +1,19 @@
 from django.forms import ModelForm
 from rsvp.models import RSVP
+from django import forms
 
 class RsvpForm(ModelForm):
     class Meta:
         model = RSVP
-        exclude = ('rsvpID', 'allowedAdults', 'allowedChildren', 'rsvpDate')
+        exclude= ('firstName', 'lastName','adultsAttending' ,'childrenAttending','status' ,'rsvpID', 'allowedAdults', 'allowedChildren', 'rsvpDate')
+    def __init__(self, *args, **kwargs):
+        max_adults = kwargs.pop('max_adults', 2)
+        max_children = kwargs.pop('max_children',0)
+
+        super(RsvpForm, self).__init__(*args, **kwargs)
+
+        adult_choices = ( (x,str(x)) for x in range(max_adults+1)) 
+        children_choices = ( (x,str(x)) for x in range(max_children+1)) 
+
+        self.fields['adults_attending'] = forms.ChoiceField(choices = adult_choices)
+        self.fields['children_attending'] = forms.ChoiceField(choices = children_choices)
